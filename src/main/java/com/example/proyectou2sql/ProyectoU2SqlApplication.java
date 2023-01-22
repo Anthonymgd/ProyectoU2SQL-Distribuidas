ad85d2c584e3472f364183065b48c4ec31bed34e
Amgoyes <amgoyes@espe.edu.ec>
2023-01-22 09:10:36 -0500

    Requisitos mínimos

---
+++ src/main/java/com/example/proyectou2sql/ProyectoU2SqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectou2sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoU2SqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoU2SqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/proyectou2sql/ProyectoU2SqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectou2sql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoU2SqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
